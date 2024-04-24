48aecd2f46ac2b89c8c19c511dfe2b37a180256c
Vitaly <82165471+VKrivkov@users.noreply.github.com>
2024-04-24 21:43:13 +0200

    Working (3 seconds to use a key)

--- Biometric/app/src/main/java/com/example/biometric/MainActivity.kt
+++ Biometric/app/src/main/java/com/example/biometric/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.biometric
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
@@ -12,7 +13,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
@@ -25,11 +25,34 @@ import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.example.biometric.ui.theme.BiometricTheme
 import java.util.concurrent.Executor
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import androidx.annotation.RequiresApi
+import java.security.KeyPairGenerator
+import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.Signature
+import kotlin.reflect.KFunction0
+import kotlin.reflect.KFunction1
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var executor: Executor
     private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var promptInfo: BiometricPrompt.PromptInfo
+    private var isAuthenticationInProgress by mutableStateOf(true) // Flag to track if authentication is in progress
+    private var currentAuthContext = AuthContext.APP_LAUNCH
+    private var initinputText by mutableStateOf("")
+    private var initoutputText by mutableStateOf("")
+
+
+
+    enum class AuthContext {
+        APP_LAUNCH,
+        SIGN_DATA
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,39 +62,70 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CenteredContent(::handleButtonClick)
-                }
+                    if (isAuthenticationInProgress) {
+                        // Show default placeholder screen while authentication is in progress
+                        DefaultScreen()
+                    } else {
+                        // Show authentication screen if authentication is successful
+                        AuthenticationScreen("", "", ::handleButtonClick, ::isKeyPairInsideSecurityHardware, ::updateSharedInputText)
+                    }               }
             }
         }
         Log.d("MainActivity", "onCreate: Starting application setup")
         executor = ContextCompat.getMainExecutor(this)
         biometricPrompt = createBiometricPrompt()
         promptInfo = createPromptInfo()
+        biometricPrompt.authenticate(promptInfo)
+        triggerBiometricAuthentication(AuthContext.APP_LAUNCH)
+
+    }
+
+    private fun triggerBiometricAuthentication(context: AuthContext) {
+        currentAuthContext = context
+        biometricPrompt.authenticate(promptInfo)
     }
 
     private fun createBiometricPrompt(): BiometricPrompt {
         val callback = object : BiometricPrompt.AuthenticationCallback() {
-
             override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
                 Log.e("BiometricPrompt", "onAuthenticationError: $errorCode, $errString")
                 super.onAuthenticationError(errorCode, errString)
-                // Handle error.
+                showAuthenticationFailedScreen()
             }
+
+            @RequiresApi(Build.VERSION_CODES.R)
             override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                Log.e("BiometricPrompt", "onAuthenticationError: Success")
                 super.onAuthenticationSucceeded(result)
-                // Handle success.
+                Log.d("BiometricPrompt", "Authentication succeeded")
+                when (currentAuthContext) {
+                    AuthContext.APP_LAUNCH -> handleAppLaunch()
+                    AuthContext.SIGN_DATA -> handleSigning()
+                }
             }
 
             override fun onAuthenticationFailed() {
                 Log.e("BiometricPrompt", "onAuthenticationError: FATAL ERROR")
                 super.onAuthenticationFailed()
-                // Handle failure.
+                showAuthenticationFailedScreen()
             }
         }
 
         return BiometricPrompt(this, executor, callback)
     }
+
+    private fun handleAppLaunch(){
+        setContent {
+            BiometricTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AuthenticationScreen("", "", ::handleButtonClick, ::isKeyPairInsideSecurityHardware, ::updateSharedInputText)
+                }
+            }
+        }
+    }
+
 
     private fun createPromptInfo(): BiometricPrompt.PromptInfo {
         return BiometricPrompt.PromptInfo.Builder()
@@ -83,17 +137,151 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Call this function when the button is clicked
-    private fun handleButtonClick(){
+    private fun handleButtonClick() {
+        currentAuthContext = AuthContext.SIGN_DATA
         biometricPrompt.authenticate(promptInfo)
     }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun handleSigning(){
+        generateKeyPair()
+        val signedData = signData(initinputText)
+        initoutputText = "Signed Output: ${signedData?.let { bytesToHex(it) }}"
+        updateUI(initoutputText)
+
+    }
+
+    private fun updateUI(text: String) {
+        setContent {
+            BiometricTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AuthenticationScreen(initinputText, text, ::handleButtonClick, ::isKeyPairInsideSecurityHardware, ::updateSharedInputText)
+                }
+            }
+        }
+    }
+
+    private fun updateSharedInputText(newText: String) {
+        initinputText = newText
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun generateKeyPair() {
+        try {
+            val keyPairGenerator = KeyPairGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_EC, "AndroidKeyStore"
+            )
+
+            val parameterSpec = KeyGenParameterSpec.Builder(
+                "biometricKeyAlias",
+                KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
+            ).apply {
+                setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
+                setUserAuthenticationRequired(true)
+                setUserAuthenticationParameters(3, KeyProperties.AUTH_BIOMETRIC_STRONG) // Adjust based on your requirement
+            }.build()
+            keyPairGenerator.initialize(parameterSpec)
+            keyPairGenerator.generateKeyPair()
+        } catch (e: Exception) {
+            Log.e("MainActivity", "Key pair generation failed", e)
+        }
+    }
+
+    private fun signData(input: String): ByteArray? {
+        val keyStore = KeyStore.getInstance("AndroidKeyStore")
+        keyStore.load(null)
+        val privateKey = keyStore.getKey("biometricKeyAlias", null) as? PrivateKey
+        val signature = Signature.getInstance("SHA256withECDSA")
+
+        return privateKey?.let {
+            signature.initSign(it)
+            signature.update(input.toByteArray())
+            signature.sign()
+        }
+    }
+
+    private fun bytesToHex(bytes: ByteArray): String {
+        val hexArray = "0123456789ABCDEF".toCharArray()
+        val hexChars = CharArray(bytes.size * 2)
+        for (j in bytes.indices) {
+            val v = bytes[j].toInt() and 0xFF
+            hexChars[j * 2] = hexArray[v ushr 4]
+            hexChars[j * 2 + 1] = hexArray[v and 0x0F]
+        }
+        return String(hexChars)
+    }
+
+    private fun showAuthenticationFailedScreen() {
+        setContent {
+            BiometricTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AuthenticationFailedScreen(::retryBiometricAuthentication)
+                }
+            }
+        }
+    }
+
+    private fun retryBiometricAuthentication() {
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+
+
+
+    private fun isKeyPairInsideSecurityHardware(): Boolean {
+        try {
+            val keyStore = KeyStore.getInstance("AndroidKeyStore")
+            keyStore.load(null)
+
+            // Retrieve the KeyGenParameterSpec from the KeyPairGenerator
+            val keyGenParameterSpec = keyStore.getKey("biometricKeyAlias", null)?.let { entry ->
+                val keyPairGenerator = KeyPairGenerator.getInstance(
+                    KeyProperties.KEY_ALGORITHM_EC, "AndroidKeyStore"
+                )
+                keyPairGenerator.initialize(null, null)
+                keyPairGenerator.javaClass.getDeclaredMethod("getKeyGenParameterSpec", entry.javaClass).invoke(keyPairGenerator, entry) as KeyGenParameterSpec
+            }
+
+            if (keyGenParameterSpec != null) {
+                return keyGenParameterSpec.isStrongBoxBacked
+            }
+
+
+        } catch (e: Exception) {
+            Log.e("MainActivity", "Error checking if key pair is inside secure hardware", e)
+        }
+        return false
+    }
+
+
+
 }
 
 
+@Composable
+fun DefaultScreen() {
+    // Placeholder screen while authentication is in progress
+    Text("Loading...")
+}
+
 
 @Composable
-fun CenteredContent(onButtonClick: () -> Unit) {
-    var inputText by remember { mutableStateOf(TextFieldValue("")) }
-    var outputText by remember { mutableStateOf("") }
+fun AuthenticationScreen(
+    initInputText: String,
+    outputText: String, // Now passed directly and used as read-only
+    onButtonClick: () -> Unit,
+    isKeyPairInsideSecurityHardware: () -> Boolean,
+    updateInputText: (String) -> Unit // Function to update text
+
+) {
+    var inputText by remember { mutableStateOf(initInputText) }
+
 
     Column(
         modifier = Modifier
@@ -110,19 +298,17 @@ fun CenteredContent(onButtonClick: () -> Unit) {
                 .border(1.dp, Color.Gray)
                 .padding(16.dp),
             decorationBox = { innerTextField ->
-                if (inputText.text.isEmpty()) {
+                if (inputText.isEmpty()) {
                     Text("Enter some text", color = Color.Gray)
                 }
                 innerTextField()
             }
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = {
-                onButtonClick()
-                outputText = "Output: ${inputText.text}"
-            }
-        ) {
+        Button(onClick ={
+            onButtonClick()
+            updateInputText(inputText)
+        }) {
             Text("Submit")
         }
         Spacer(modifier = Modifier.height(16.dp))
@@ -132,17 +318,34 @@ fun CenteredContent(onButtonClick: () -> Unit) {
                 modifier = Modifier.border(1.dp, Color.Gray).padding(16.dp)
             )
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            val info = isKeyPairInsideSecurityHardware()
+            Log.e("ORIGIN VERIFICATION", info.toString())
+        }) {
+            Text("Verify")
+        }
     }
 }
 
 
 
-
-@Preview(showBackground = true)
 @Composable
-fun CenteredContentPreview() {
-    BiometricTheme {
-        // Pass a dummy lambda function that does nothing on button click
-        CenteredContent(onButtonClick = {})
+fun AuthenticationFailedScreen(onRetryClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Authentication failed, please try again")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onRetryClick) {
+            Text("Retry Authentication")
+        }
     }
 }
+
+
